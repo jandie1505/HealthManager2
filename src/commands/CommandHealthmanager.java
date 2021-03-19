5e91a1186fa0cb44d576c5f77e254d752442059f
jandie1505 <jande1234567890@gmail.com>
2021-03-19 17:13:01 +0100

    Finished /heal Command Added /healthmanager help Subcommand

--- src/commands/CommandHealthmanager.java
+++ src/commands/CommandHealthmanager.java
@@ -10,16 +10,39 @@ import org.bukkit.entity.Player;
 
 public class CommandHealthmanager implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if(sender instanceof Player){
-            Player p = (Player) sender;
-            p.sendMessage(ChatMessages.stateMessage1);
-            p.sendMessage(ChatMessages.stateMessage2);
-        } else {
-            ConsoleMessages.defaultMessage("HealthManager2 by jandie1505");
-            ConsoleMessages.defaultMessage("Version: " + Main.version);
-            ConsoleMessages.defaultMessage("Website: jandie1505.net");
-            ConsoleMessages.defaultMessage("Use /healthmanager help for more info");
+        Player p = (Player) sender;
+        if(args.length == 0){
+            if(sender instanceof Player){
+                p.sendMessage(ChatMessages.stateMessage1);
+                p.sendMessage(ChatMessages.stateMessage2);
+            } else {
+                ConsoleMessages.defaultMessage("HealthManager2 by jandie1505");
+                ConsoleMessages.defaultMessage("Version: " + Main.version);
+                ConsoleMessages.defaultMessage("Website: jandie1505.net");
+                ConsoleMessages.defaultMessage("Use /healthmanager help for more info");
+            }
+        } else if(args.length == 1){
+            switch (args[0]){
+                case "help":
+                    if(sender instanceof Player){
+                        for(int i = 0;i<=12;i++){
+                            p.sendMessage(ChatMessages.helpMessage[i]);
+                        }
+                    } else {
+                        ConsoleMessages.helpMessage();
+                    }
+                    break;
+                default:
+
+                    if(sender instanceof Player){
+                        p.sendMessage(ChatMessages.commandNotFound);
+                    } else {
+                        ConsoleMessages.defaultMessage("Command not found");
+                    }
+            }
         }
+
+
 
 
         return true;
--- src/main/ChatMessages.java
+++ src/main/ChatMessages.java
@@ -7,19 +7,41 @@ public class ChatMessages {
 
     public static String prefix;
     public static String nopermission;
+    public static String commandNotFound;
+
     public static String stateMessage1;
     public static String stateMessage2;
+
+    public static String helpMessage[] = new String[13];
+
     public static String playernotfound;
     public static String healmessage;
     public static String healmessageothers;
 
     public static void load(){
         prefix = config.getString("prefix");
-        nopermission = config.getString("nopermission");
-        stateMessage1 = "§6HealthManager2 (" + Main.version + ") by jandie1505";
-        stateMessage2 = "§eUse /healthmanager help for more information";
-        healmessage = config.getString("healmessage");
-        healmessageothers = config.getString("healmessageothers");
-        playernotfound = config.getString("playernotfound");
+        nopermission = ChatMessages.prefix + config.getString("nopermission");
+        commandNotFound = ChatMessages.prefix + config.getString("commandnotfound");
+
+        stateMessage1 = ChatMessages.prefix + "§6HealthManager2 (" + Main.version + ") by jandie1505";
+        stateMessage2 = ChatMessages.prefix + "§eUse /healthmanager help for more information";
+
+        helpMessage[0] = ChatMessages.prefix + "§eHealthManager2 Help:";
+        helpMessage[1] = ChatMessages.prefix + "§7/healthmanager - Status command";
+        helpMessage[2] = ChatMessages.prefix + "§7/heal ([Player]) - Heal yourself (or a specific player)";
+        helpMessage[3] = ChatMessages.prefix + "§7/sethealth [Player] [Amount] - Set the health level of a specific player";
+        helpMessage[4] = ChatMessages.prefix + "§7/setmaxhealth [Player] [Amount] - Set the max health level of a specific player";
+        helpMessage[5] = ChatMessages.prefix + "§7/gethealth [Player] - See the health level of a specific player";
+        helpMessage[6] = ChatMessages.prefix + "§7/feed ([Player]) - Feed yourself (or a specific player)";
+        helpMessage[7] = ChatMessages.prefix + "§7/sethunger [Player] - Set the food level of a specific player";
+        helpMessage[8] = ChatMessages.prefix + "§7/gethunger [Player] - Get the food level of a specific player";
+        helpMessage[9] = ChatMessages.prefix + "§7/setsaturation [Player] - Set the saturation level of a specific player";
+        helpMessage[10] = ChatMessages.prefix + "§7/getsaturation [Player] - Get the saturation level of a specific player";
+        helpMessage[11] = ChatMessages.prefix + "§7/saturation ([Player]) - Turn on/off saturation mode for yourself (or a specific player)";
+        helpMessage[12] = ChatMessages.prefix + "§7/god ([Player]) - Turn on/off god mode for yourself (or a specific player)";
+
+                healmessage = ChatMessages.prefix + config.getString("healmessage");
+        healmessageothers = ChatMessages.prefix + config.getString("healmessageothers");
+        playernotfound = ChatMessages.prefix + config.getString("playernotfound");
     }
 }
